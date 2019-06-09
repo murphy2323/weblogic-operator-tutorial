491555b6991c20fb4a97c26a35a704406b354e50
Bob Murphy <murphy2323@gmail.com>
2019-06-09 17:52:02 -0400

    ...

--- scripts/model.py
+++ scripts/model.py
@@ -124,10 +124,10 @@ set('TestTableName', "SQL ISVALID")
 
 print 'create JDBCDriverParams Properties'
 
-cd('/JDBCSystemResources/' + dsname + '/JDBCResource/' + dsname + '/JDBCDriverParams/' + dsname + '/Properties/' + dsname )
+cd('/JDBCSystemResource/' + dsname + '/JdbcResource/' + dsname + '/JDBCDriverParams/' + dsname + '/Properties/' + dsname )
 cmo.createProperty("javax.net.ssl.keyStoreType")
 
-cd('/JDBCSystemResources/' + dsname + '/JDBCResource/' + dsname + '/JDBCDriverParams/' + dsname + '/Properties/' + dsname + "/Property/javax.net.ssl.keyStoreType")
+cd('/JDBCSystemResource/' + dsname + '/JdbcResource/' + dsname + '/JDBCDriverParams/' + dsname + '/Properties/' + dsname + "/Property/javax.net.ssl.keyStoreType")
 cmo.setValue("JKS")
 
 
