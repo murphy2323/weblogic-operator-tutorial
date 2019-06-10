1fbc49881bf2b922415cd652c2b70d58b9d4774e
Bob Murphy <murphy2323@gmail.com>
2019-06-09 20:24:34 -0400

    ...

--- scripts/model.py
+++ scripts/model.py
@@ -123,7 +123,7 @@ set('MaxCapacity', 25)
 set('TestTableName', "SQL ISVALID")
 
 print 'create JDBCDriverParams Properties'
-cd('/JDBCSystemResource/' + dsname + '/JdbcResource/' + dsname + '/JdbcDriverParams/'
+cd('/JDBCSystemResource/' + dsname + '/JdbcResource/' + dsname + '/JdbcDriverParams/')
 create('myProperties','Properties')
 #cd('Properties/NO_NAME_0')
 
